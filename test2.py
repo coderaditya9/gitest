de122c152ce4055babf86a1cd36791f3d319cd0f
aditya <uk09army@gmail.com>
2026-08-09 08:28:52 +0530

    new program

--- test2.py
+++ test2.py
@@ -1,3 +1,9 @@
 print("my name is aditya ")
 print("i am pursuing bca in 2025")
 print("i am 18 year old boy")
+
+
+num = int(input("enter the number : "))
+num1 = int(input("enter the number : "))
+
+print (num + num1)
