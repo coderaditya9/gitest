2cba9844b13b33ae30bb708e9f1becac56fccb65
aditya <uk09army@gmail.com>
2026-08-10 21:01:56 +0530

    new code1

--- demo.py
+++ demo.py
@@ -7,28 +7,35 @@ while True:
 
     choice = int(input("enter the choice : "))
 
+    #addition
     if choice == 1:
         num = int(input("enter the number : "))
         num1 = int(input("enter the number : "))
         print("")
         print("the addition number : ", num + num1)
 
+    #subtraction
     elif choice == 2:
         num = int(input("enter the number : "))
         num1 = int(input("enter the number : "))
         print("")
         print("the subtraction number : ", num - num1)
-
+    #multiplication
     elif choice == 3:
         num = int(input("enter the number : "))
         num1 = int(input("enter the number : "))
         print("")
         print("the multiliccation number : ", num * num1)
 
+    #division
     elif choice == 4:
         num = int(input("enter the number : "))
         num1 = int(input("enter the number : "))
         print("")
         print("the division number : ", num / num1)
+    #exit
+    elif choice == 5:
+        print("program exited")
+        break
     else:
         print("invalid choice ! program exited")
