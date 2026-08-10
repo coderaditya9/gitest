2ae87dfdc04a08a82ea78770e409c23e4f18f101
aditya <uk09army@gmail.com>
2026-08-10 20:54:55 +0530

    new code

--- demo.py
+++ demo.py
@@ -1,11 +1,34 @@
-def my_function():
-    a = 5
-    b = 6
-    c = 9
+while True:
+    print("1. ADDITION ")
+    print("2. SUBTRACT ")
+    print("3. MULTIPLY ")
+    print("4. DIVIDE ")
+    print("5. EXIT ")
 
-    return d
+    choice = int(input("enter the choice : "))
 
-my_function()
+    if choice == 1:
+        num = int(input("enter the number : "))
+        num1 = int(input("enter the number : "))
+        print("")
+        print("the addition number : ", num + num1)
 
+    elif choice == 2:
+        num = int(input("enter the number : "))
+        num1 = int(input("enter the number : "))
+        print("")
+        print("the subtraction number : ", num - num1)
 
-test()
+    elif choice == 3:
+        num = int(input("enter the number : "))
+        num1 = int(input("enter the number : "))
+        print("")
+        print("the multiliccation number : ", num * num1)
+
+    elif choice == 4:
+        num = int(input("enter the number : "))
+        num1 = int(input("enter the number : "))
+        print("")
+        print("the division number : ", num / num1)
+    else:
+        print("invalid choice ! program exited")
