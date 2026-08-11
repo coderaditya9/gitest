192a8daa38e52caaf936f5c7c8425714ca802c05
aditya <uk09army@gmail.com>
2026-08-11 20:30:22 +0530

    changes forever

--- demo.py
+++ demo.py
@@ -1,41 +1,46 @@
+"""A simple menu-driven calculator program."""
+
 while True:
-    print("1. ADDITION ")
-    print("2. SUBTRACT ")
-    print("3. MULTIPLY ")
-    print("4. DIVIDE ")
-    print("5. EXIT ")
+    print("1. ADDITION")
+    print("2. SUBTRACT")
+    print("3. MULTIPLY")
+    print("4. DIVIDE")
+    print("5. EXIT")
 
-    choice = int(input("enter the choice : "))
+    choice = int(input("Enter the choice: "))
 
-    #addition
+    # Addition
     if choice == 1:
-        num = int(input("enter the number : "))
-        num1 = int(input("enter the number : "))
+        num = int(input("Enter the number: "))
+        num1 = int(input("Enter the number: "))
         print("")
-        print("the addition number : ", num + num1)
+        print("The addition number:", num + num1)
 
-    #subtraction
+    # Subtraction
     elif choice == 2:
-        num = int(input("enter the number : "))
-        num1 = int(input("enter the number : "))
+        num = int(input("Enter the number: "))
+        num1 = int(input("Enter the number: "))
         print("")
-        print("the subtraction number : ", num - num1)
-    #multiplication
-    elif choice == 3:
-        num = int(input("enter the number : "))
-        num1 = int(input("enter the number : "))
-        print("")
-        print("the multiliccation number : ", num * num1)
+        print("The subtraction number:", num - num1)
 
-    #division
-    elif choice == 4:
-        num = int(input("enter the number : "))
-        num1 = int(input("enter the number : "))
+    # Multiplication
+    elif choice == 3:
+        num = int(input("Enter the number: "))
+        num1 = int(input("Enter the number: "))
         print("")
-        print("the division number : ", num / num1)
-    #exit
+        print("The multiplication number:", num * num1)
+
+    # Division
+    elif choice == 4:
+        num = int(input("Enter the number: "))
+        num1 = int(input("Enter the number: "))
+        print("")
+        print("The division number:", num / num1)
+
+    # Exit
     elif choice == 5:
-        print("program exited")
+        print("Program exited")
         break
+
     else:
-        print("invalid choice ! program exited")
+        print("Invalid choice!")
